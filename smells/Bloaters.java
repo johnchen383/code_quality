08aaa36086c2932ff319ef85d61ede046a9ebf32
John Chen <john.bm.chen@gmail.com>
2021-11-10 17:42:15 +1300

    feat: primitive obsession

--- smells/Bloaters.java
+++ smells/Bloaters.java
@@ -1,5 +1,28 @@
 package smells;
 
 public class Bloaters {
+
+    /**
+     * PRIMITIVE OBSESSION      "Everything's a primitive"
+     */
+    public class Person {
+        String name;
+        String address;
+        String password;
     
+        public String getPostCode(){
+            String postCode = address.substring(address.length() - 4);
+            return postCode;
+        }
+    
+        public String getLastName() {
+            String lastName = name.substring(name.lastIndexOf(" ") + 1);
+            return lastName;
+        }
+        
+        public boolean checkPasswordLength() {
+            return password.length() >= 8;
+        }
+    }
 }
+
