b4e137b395b29e18f9037e36c02dfbf38c8911d0
John Chen <john.bm.chen@gmail.com>
2021-11-10 13:03:50 +1300

    feat: maintability.java comments written

---
+++ Maintainability.java
@@ -0,0 +1,22 @@
+/**
+ * Maintainability is defined as degree of effectiveness and efficiency a codebase can be maintained over time (improved, corrected, or adapted). Sub-characteristics include modularity, reusability, analysability, modifiability, testability.
+ */
+public class Maintainability {
+    /**
+     * Naming
+     * - Intention Revealing Names
+     * - Avoid disinformation
+     * - Meaningful differences
+     * - Pronounceable
+     * - Searchable (No 'magic' numbers)
+     * - Avoid encodings
+     * - Be consistent
+     * - Type specific (verb [methods], noun [object, fields], "is" prefix [boolean])
+     */
+
+
+     /**
+      * Abstraction --> use a step-down rule, high-level through to low-level.
+      * - Functions should be small ~ 4 lines
+      */
+}
