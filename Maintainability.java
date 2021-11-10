08b375edb6a7bda79259f5d42bfd587f72c9c9f4
John Chen <john.bm.chen@gmail.com>
2021-11-10 13:43:03 +1300

    feat: maintainability + readability done

--- Maintainability.java
+++ Maintainability.java
@@ -1,17 +1,21 @@
 /**
- * Maintainability is defined as degree of effectiveness and efficiency a codebase can be maintained over time (improved, corrected, or adapted). Sub-characteristics include modularity, reusability, analysability, modifiability, testability.
+ * Maintainability is defined as degree of effectiveness and efficiency a
+ * codebase can be maintained over time (improved, corrected, or adapted).
+ * Sub-characteristics include modularity, reusability, analysability,
+ * modifiability, testability.
  */
 public class Maintainability {
     /**
      * Naming
-     * - Intention Revealing Names
-     * - Avoid disinformation
-     * - Meaningful differences
-     * - Pronounceable
-     * - Searchable (No 'magic' numbers)
-     * - Avoid encodings
-     * - Be consistent
+     * - Intention Revealing Names              getThem()
+     * - Avoid disinformation                   don't say what it's not
+     * - Meaningful differences                 productInfo vs productData
+     * - Pronounceable                          
+     * - Searchable                             no 'magic' numbers
+     * - Avoid encodings                        phoneString
+     * - Be consistent                          fetch, get, retrieve
      * - Type specific (verb [methods], noun [object, fields], "is" prefix [boolean])
+     * - Naming conventions (PascalCase for classes, camelCase for others, SCREAMING_SNAKE_CASE for constants)
      */
 
 
--- Readability.java
+++ Readability.java
@@ -1,3 +1,23 @@
 public class Readability {
+    /**
+     * Nested conditionals should be removed
+     */
     
+    /**
+     * Comments     add clutter, potential misinformation, add to maintenance overhead
+     * Good for:    legal, intention revealing, clarification, design rationale, workarounds
+     */
+
+     /**
+      * Code conventions
+      * - Braces used when optional               if(true){ x }
+      * - Kernighan and Ritchie style braces      usual manner of using braces
+      * - Indentation                             4+ and 2 spaces for blocks [just use Shift+Alt+F]
+      * - Single statement per line
+      * - Line width is 100 characters            Shift+Alt+F
+      * - Group boolean logic / arithmetic
+      * - One class per file
+      * - No wildcard imports
+      * - Use whitespacing and logical grouping of related methods and variables
+      */
 }
