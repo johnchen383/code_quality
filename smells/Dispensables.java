f9cbda4bd726010c0de29ebc3c1ff0c4b25c981c
John Chen <john.bm.chen@gmail.com>
2021-11-11 22:58:53 +1300

    feat: dispensables

--- smells/Dispensables.java
+++ smells/Dispensables.java
@@ -1,5 +1,30 @@
 package smells;
 
 public class Dispensables {
-    
+    /**
+     * LAZY CLASS "Whole class is lazy. Adds no value. Middle Man can be seen as a
+     * subset of Lazy Class" Refactor: remove class
+     */
+
+    /**
+     * SPECULATIVE GENERALITY "Adding what might be useful in the future and making
+     * present job harder"
+     */
+
+    /**
+     * DATA CLASS. "Containers for data only + getters/setters/fields. No real methods"
+     * Refactor: Extract?
+     */
+
+    /**
+     * DUPLICATED CODE. 
+     * Refactor: remove method/statement
+     */
+    public int areaBigSquare(int side){
+        return side * side;
+    }
+
+    public int areaSmallSquare(int side){
+        return side * side;
+    }
 }
