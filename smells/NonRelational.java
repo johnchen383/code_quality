7b4a2b39820f4e4a7b804731432ab45e9cd34325
John Chen <john.bm.chen@gmail.com>
2021-11-11 23:29:17 +1300

    feat: non-relational code smells grouped

--- smells/NonRelational.java
+++ smells/NonRelational.java
@@ -7,5 +7,69 @@ package smells;
  * assessments.
  */
 public class NonRelational {
-    
+    /**
+     * ABUSERS:
+     * - Temporary Field
+     * - Switch statements OR long if's
+     */
+
+    /**
+     * BLOATERS:
+     * - Long Method
+     * - Long Parameter List
+     * - Primitive Obsession
+     * - Data Clumps
+     */
+
+    /**
+     * COUPLERS:
+     * - Message Chains
+     */
+
+    /**
+     * DISPENSABLES:
+     * - Speculative Generality
+     * - Duplicated Code
+     */
+
+    /**
+     * CHANGE PREVENTERS:
+     * - Divergent Change
+     */
+
+    /**
+     * Just in case, it's needed ahah
+     */
+    public class Relational {
+        /**
+         * ABUSERS:
+         * - Alternative Classes [doing the same thing]
+         * - Refused Bequest [unneeded inheritance]
+         */
+
+        /**
+         * BLOATERS:
+         * - Large Class
+         */
+
+        /**
+         * COUPLERS:
+         * - Middle Man
+         * - Feature Envy
+         * - Inappropriate Intimacy
+         */
+
+        /**
+         * DISPENSABLES:
+         * - Lazy Class
+         * - Data Class
+         */
+        
+        /**
+         * CHANGE PREVENTERS:
+         * - Shotgun Surgery
+         * - Parallel Inheritance Heirarchies
+         */
+        
+    }
 }
