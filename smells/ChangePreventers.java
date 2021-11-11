d057cc770807d8015ab24dc435ef89980ad99778
John Chen <john.bm.chen@gmail.com>
2021-11-11 23:04:13 +1300

    feat: preventers

---
+++ smells/ChangePreventers.java
@@ -0,0 +1,20 @@
+package smells;
+
+public class ChangePreventers {
+    /**
+     * DIVERGENT CHANGE. Making one change requires changing a lot of methods within
+     * a class.
+     * 
+     */
+
+    /**
+     * SHOTGUN SURGERY. Making one change requires a lot of small changes across
+     * classes.
+     */
+
+    /**
+     * PARALLEL INHERITANCE HEIRARCHIES. Adding onto one hierarchy forces you to add
+     * another subclass to a parallel inheritance hierarchy.
+     * 
+     */
+}
